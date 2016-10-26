6dde5adefccedc6e8153cb9a269185dd97e0313a
tangchuny <442593738@qq.com>
2016-10-26 12:07:47 +0800

    demo
    demo

---
+++ app/components/productBox.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ProductBox;
+ProductBox = React.createClass({
+    render: function () {
+        return (
+            <div className="productBox">
+                hello react
+            </div>
+        );
+    }
+});
+
+module.exports = ProductBox;
---
+++ app/main.js
@@ -0,0 +1,6 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let AppComponent = require('./components/productBox.js');
+let seconds = require('./components/second.js');
+
+ReactDOM.render(<seconds/>, document.write("Webpack for the win!"));
